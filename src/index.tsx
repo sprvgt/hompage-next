5b0a8bdd56c41dacc7ee23a83403b7e92576847b
sprvgt <11452617+sprvgt@users.noreply.github.com>
2022-08-21 18:58:41 -0700

    Add hyperlinks to the intro.

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import profileImage from './images/me.jpeg';
 
 function ProfileImage() {
-  return <img src={profileImage} className="profile-image" alt="Profile Image" />;
+  return <img 
+      src={profileImage} className="profile-image" alt="Profile Image" />;
 }
 
 function Introduction() {
@@ -12,12 +13,22 @@ function Introduction() {
     <div className="introduction">
       <h1 className="name">Xuan Lu (陆璇) </h1>
       <div className="intro-first">
-        I am a Research Fellow in the School of Information at the University of Michigan. I work with 
-        Prof. Qiaozhu Mei in the Foreseer Group.
+        I am a Research Fellow in the <a 
+            href="https://www.si.umich.edu/">School of Information</a> at
+        the <a
+            href="https://www.umich.edu/">University of Michigan</a>. I work
+        with Prof. <a 
+            href="http://www-personal.umich.edu/~qmei/">Qiaozhu Mei</a> in
+        the <a 
+            href="http://foreseer.si.umich.edu/">Foreseer Group</a>.
       </div>
       <div className="intro-second">
-        I was fortunate to be advised by Prof. Hong Mei and Prof. Xuanzhe Liu and received my Ph.D. from 
-        Peking University. 
+        I was fortunate to be advised by Prof. <a 
+            href="http://sei.pku.edu.cn/~meih/index_en.html">Hong Mei</a> and
+        Prof. <a
+            href="http://liuxuanzhe.com/">Xuanzhe Liu</a> and received my Ph.D 
+        from <a 
+            href="https://english.pku.edu.cn/">Peking University</a>. 
       </div>
     </div>
   )
