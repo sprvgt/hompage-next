6d0bca5c6e20d716759de3e6b73da98a2956b11c
sprvgt <11452617+sprvgt@users.noreply.github.com>
2022-08-21 18:23:33 -0700

    Update profile section with responsive layout.

--- src/index.tsx
+++ src/index.tsx
@@ -1,170 +1,38 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Interface for Square's properties.
-interface SquareProps {
-  value?: string;
+import profileImage from './images/me.jpeg';
 
-  onClick: () => void;
+function ProfileImage() {
+  return <img src={profileImage} className="profile-image" alt="Profile Image" />;
 }
 
-function Square(props: SquareProps) {
+function Introduction() {
   return (
-    <button 
-        className="square"
-        onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-type SquareSnapshot = Array<string | undefined>;
-
-interface BoardProps {
-  squares: SquareSnapshot;
-
-  onClick: (i: number) => void;
-}
-
-class Board extends React.Component<BoardProps> {
-  renderSquare(i: number) {
-    return (
-      <Square value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}/>
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+    <div className="introduction">
+      <h1 className="name">Xuan Lu (陆璇) </h1>
+      <div className="intro-first">
+        I am a Research Fellow in the School of Information at the University of Michigan. I work with 
+        Prof. Qiaozhu Mei in the Foreseer Group.
       </div>
-    );
-  }
-}
-
-interface GameState {
-  history: Array<SquareSnapshot>;
-  currentStep: number;
-  xIsNext: boolean;
-}
-
-class Game extends React.Component<{}, GameState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      history: [
-        Array(9).fill(undefined),
-      ],
-      currentStep: 0,
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.currentStep + 1);
-    const current = history[this.state.currentStep];
-    if (calculateWinner(current) || current[i]) {
-      return;
-    }
-    const squares = current.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([squares]),
-      currentStep: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step: number) {
-    const history = this.state.history;
-    this.setState({
-      history: history.slice(0, step + 1),
-      currentStep: step,
-      xIsNext: step % 2 === 0 ? true : false,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current);
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-          'Go to move #' + move
-          : 'Go to game start';
-
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div className="game">
-        <p>Homepage</p>
-        <div className="game-board">
-          <Board squares = {current}
-              onClick = {(i) => this.handleClick(i)}/>
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
+      <div className="intro-second">
+        I was fortunate to be advised by Prof. Hong Mei and Prof. Xuanzhe Liu and received my Ph.D. from 
+        Peking University. 
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-// ========================================
+function Profile() {
+  return (
+    <div className="profile">
+      <ProfileImage />
+      <Introduction />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <Game />,
+  <Profile />,
   document.getElementById('root')
 );
-
-function calculateWinner(squares: SquareSnapshot) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] 
-      && squares[a] === squares[b]
-      && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return undefined;
-}
