cf8e42fb4cc8733920c1677eb2e4833b17395756
sprvgt <11452617+sprvgt@users.noreply.github.com>
2022-09-15 21:51:32 -0700

    Add sections with static content: Education, Research Interests, Academic Services, Honor and Awards.

--- src/index.tsx
+++ src/index.tsx
@@ -45,7 +45,99 @@ function Profile() {
   )
 }
 
+function Education() {
+  return  (
+    <div className="segment">
+      <div className="segment-title">
+        Education
+      </div>
+      <ul>
+        <li>2016-2019, Ph.D. student, in Computer Science, Peking University</li>
+        <li>2013-2016, Master student (advisor: Prof. Xuanzhe Liu), in Computer Science, Peking University</li>
+        <li>2010-2013, B.A. in Economics (dual degree), Peking University</li>
+        <li>2009-2013, B.S. in Computer Science, Peking University</li>
+      </ul>
+    </div>
+  )
+}
+
+function ResearchInterests() {
+  return (
+    <div className="segment">
+      <div className="segment-title">
+        Research Interests
+      </div>
+      <div className="segment-content">
+        Causal data science; Emoji; Mobile app ecosystem
+      </div>
+    </div>
+  )
+}
+
+function AcademicServices() {
+  return  (
+    <div className="segment">
+      <div className="segment-title">
+        Academic Services
+      </div>
+      <ul>
+        <li>PC member, ACM International WSDM Conference (WSDM, 2021, 2022)</li>
+        <li>PC member, ACM International Conference on Information and Knowledge Management (CIKM, 2021, 2022)</li>
+        <li>PC member, ACM SIGKDD Conference on Knowledge Discovery and Data Mining, Applied Data Science Track (KDD ADS track, 2021, 2022)</li>
+        <li>PC member, International Workshop on Emoji Understanding and Applications in Social Media (EMOJI, 2021, 2022)</li>
+        <li>PC member, International ACM SIGIR Conference on Research and Development in Information Retrieval, short paper (SIGIR short paper, 2021)</li>
+        <li>PC member, IEEE International Conference on Cloud Computing (IEEE CLOUD, 2020)</li>
+        <li>Reviewer for IEEE Transactions on Services Computing (TSC, 2021, 2022)</li>
+        <li>Reviewer for IEEE Transactions on Software Engineering (TSE, 2021)</li>
+        <li>Reviewer for ACM Transactions on Intelligent Systems and Technology (TIST, 2021)</li>
+        <li>Reviewer for Frontiers in Big Data (2021)</li>
+        <li>Reviewer for International Journal of Human-Computer Interaction (IJHCI, 2021)</li>
+        <li>Reviewer for ACM Transactions on the Web (TWEB, 2020, 2022)</li>
+        <li>Reviewer for Journal of Biomedical Informatics (JBI, 2020)</li>
+        <li>Reviewer for Proceedings of the ACM on Interactive, Mobile, Wearable and Ubiquitous Technologies (IMWUT, 2018)</li>
+        <li>External reviewer for ACM SIGKDD conference on Knowledge Discovery and Data Mining (KDD 2017, 2018, 2019)</li>
+        <li>External reviewer for International Conference on Mining Software Repositories (MSR 2017)</li>
+      </ul>
+    </div>
+  )
+}
+
+function Awards() {
+  return (
+    <div className="segment">
+      <div className="segment-title">
+        Honors and Awards      
+      </div>
+      <ul>
+        <li>2018, Yang Fuqing & Wang Yangyuan Academician Scholarship, Peking University</li>
+        <li>2017, Microsoft Research Asia Fellowship Award, Microsoft Research Asia</li>
+        <li>2017, National Scholarship, Ministry of Education</li>
+        <li>2016, Lee Wai Wing Scholarship, Peking University</li>
+        <li>2015, Excellent Research Award, Peking University</li>
+        <li>2014, Suzhou Industrial Park Scholarship, Peking University</li>
+        <li>2013, Outstanding Graduate of Beijing</li>
+        <li>2013, Outstanding Graduate of Peking University</li>
+        <li>2012, National Scholarship, Ministry of Education)</li>
+        <li>2011, Outstanding Student Leader of Peking University</li>
+        <li>2010, Suzhou Industrial Park Scholarship, Peking University</li>
+      </ul>
+    </div>
+  )
+}
+
+function Main() {
+  return (
+    <div>
+      <Profile />
+      <Education />
+      <ResearchInterests />
+      <AcademicServices />
+      <Awards />
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <Profile />,
+  <Main />,
   document.getElementById('root')
 );
